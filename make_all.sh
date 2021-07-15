c0f47084c7c9c91637e7556fb2ba8a895a5c1d9f
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-15 17:14:37 +0200

    Master build script + some 5.10.50 linux kernel patch fixes

--- make_all.sh
+++ make_all.sh
@@ -14,32 +14,32 @@ source ./gh_token
 KERNEL_VERSION="5.10.50"
 
 # Amlogic kernels
-#make kernel-s905-build
+make kernel-s905-build
 make kernel-s912-build
 make kernel-s922x-build
 make kernel-sm1-build
-#TARGET=s905 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-TARGET=s912 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=s905  VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=s912  VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=s922x VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-TARGET=sm1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=sm1   VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Rockchip kernels
 make kernel-rk3288-build
-#make kernel-rk3326-build
+make kernel-rk3326-build
 make kernel-rk3399-build
 TARGET=rk3288 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rk3326 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=rk3326 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=rk3399 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Raspberry Pi kernels
-#make kernel-rpi1-build
-#make kernel-rpi2-build
-#make kernel-rpi3-build
-#make kernel-rpi4-build
-#TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-rpi1-build
+make kernel-rpi2-build
+make kernel-rpi3-build
+make kernel-rpi4-build
+TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Samsung kernels
 make kernel-exynos5422-build
