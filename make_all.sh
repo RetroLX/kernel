523e7861b497385b7847708c810ab0ae78875a09
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-18 16:12:58 +0200

    Makefile update

--- make_all.sh
+++ make_all.sh
@@ -7,8 +7,9 @@ KERNEL_VERSION=$(date '+%Y-%m-%d')
 
 # Custom kernels based on current date
 make kernel-s812-build
-#make kernel-h616-build
+make kernel-h616-build
 TARGET=s812 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=h616 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Mainline kernels based on official version
 KERNEL_VERSION="5.10.50"
@@ -29,14 +30,6 @@ TARGET=s912  VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=s922x VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=sm1   VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
-# Rockchip kernels
-make kernel-rk3288-build
-make kernel-rk3326-build
-make kernel-rk3399-build
-TARGET=rk3288 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-TARGET=rk3326 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-TARGET=rk3399 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-
 # Raspberry Pi kernels
 make kernel-rpi1-build
 make kernel-rpi2-build
@@ -46,6 +39,14 @@ TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+
+# Rockchip kernels
+make kernel-rk3288-build
+make kernel-rk3326-build
+make kernel-rk3399-build
+TARGET=rk3288 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=rk3326 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+TARGET=rk3399 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Samsung kernels
 make kernel-exynos5422-build
