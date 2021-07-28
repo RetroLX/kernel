80c00a44fb547a035043aaac70a8e0cc08fe3d9e
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-29 00:15:54 +0200

    Bump and cleanup

--- make_all.sh
+++ make_all.sh
@@ -6,18 +6,18 @@ source ./gh_token
 # Custom kernels based on current date
 KERNEL_VERSION=$(date '+%Y-%m-%d')
 #make kernel-s812-build
-#make kernel-h616-build
 #TARGET=s812 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+#make kernel-h616-build
 #TARGET=h616 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Mainline kernels based on official version
 KERNEL_VERSION="5.10.54"
 
 # Allwinner kernels
-#make kernel-h3-build
-#TARGET=h3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#make kernel-h5-build
-#TARGET=h5 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-h3-build
+TARGET=h3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-h5-build
+TARGET=h5 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Amlogic kernels
 make kernel-s905-build
@@ -41,15 +41,22 @@ TARGET=rk3399 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 make kernel-exynos5422-build
 TARGET=exynos5422 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
+# Generic x86/x86_64 kernels
+KERNEL_VERSION="5.13.6"
+make kernel-x86-build
+TARGET=x86 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-x86_64-build
+TARGET=x86_64 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+
 # Raspberry kernels based on official version
-KERNEL_VERSION="5.10.50"
+KERNEL_VERSION="5.10.52"
 
 # Raspberry Pi kernels
-#make kernel-rpi1-build
-#make kernel-rpi2-build
-#make kernel-rpi3-build
-#make kernel-rpi4-build
-#TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-#TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-rpi1-build
+TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-rpi2-build
+TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-rpi3-build
+TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+make kernel-rpi4-build
+TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
