cf6e0efe19547e31e9050e098a22c8bbf713128a
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-27 16:13:00 +0200

    Bump mainline LTS kernels to 5.10.76

--- make_all.sh
+++ make_all.sh
@@ -13,7 +13,7 @@ make kernel-rk356x-build
 TARGET=rk356x VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Mainline kernels based on official version
-KERNEL_VERSION="5.10.75"
+KERNEL_VERSION="5.10.76"
 
 # Allwinner kernels
 make kernel-aw32-build
