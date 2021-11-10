bd82f8dcce5aa4376123818616c816a3c075419c
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-10 01:04:15 +0100

    Tidy up 5.15 kernel branch

--- make_all.sh
+++ make_all.sh
@@ -4,12 +4,12 @@
 source ./gh_token
 
 # Custom kernels based on current date
-KERNEL_VERSION=$(date '+%Y-%m-%d')
-make kernel-rk356x-build
-TARGET=rk356x VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+#KERNEL_VERSION=$(date '+%Y-%m-%d')
+#make kernel-rk356x-build
+#TARGET=rk356x VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Mainline kernels based on official version
-KERNEL_VERSION="5.15-test"
+KERNEL_VERSION="5.15.1"
 
 # Allwinner kernels
 make kernel-aw32-build
@@ -44,21 +44,20 @@ make kernel-exynos5422-build
 TARGET=exynos5422 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Generic x86/x86_64 kernels
-KERNEL_VERSION="5.14.15"
 make kernel-x86-build
 TARGET=x86 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 make kernel-x86_64-build
 TARGET=x86_64 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
 
 # Raspberry kernels based on official version
-KERNEL_VERSION="5.10.71"
+#KERNEL_VERSION="5.10.71"
 
 # Raspberry Pi kernels
-make kernel-rpi1-build
-TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-make kernel-rpi2-build
-TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-make kernel-rpi3-build
-TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
-make kernel-rpi4-build
-TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+#make kernel-rpi1-build
+#TARGET=rpi1 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+#make kernel-rpi2-build
+#TARGET=rpi2 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+#make kernel-rpi3-build
+#TARGET=rpi3 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
+#make kernel-rpi4-build
+#TARGET=rpi4 VERSION=${KERNEL_VERSION} ./upload_kernel.sh
