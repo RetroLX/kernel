11453468ebbf0eb3182cc71f87d4fb58706afb38
lilo <lilo@lilo-co.almostmy.com>
2021-11-15 22:48:01 +0100

    CI WIP by Lilo

--- ci-docker/pipeline-kernel-aw32.sh
+++ ci-docker/pipeline-kernel-aw32.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Builds a RetroLX kernel
 
+date
 cd /ci-docker
 git clone https://github.com/RetroLX/kernel.git
 cd kernel
 git submodule init
 git submodule update
 make kernel-aw32-build
+date
