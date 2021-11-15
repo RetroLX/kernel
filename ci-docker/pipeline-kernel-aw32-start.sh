b29762e8fa1c59278cb881e154a83303e317b5c7
lilo <lilo@lilo-co.almostmy.com>
2021-11-15 22:02:34 +0100

    CI WIP by Lilo

--- ci-docker/pipeline-kernel-aw32-start.sh
+++ ci-docker/pipeline-kernel-aw32-start.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$SCRIPT_DIR"
 
 docker pull retrolx/build-environment-ubuntu-20.10
-docker run -v "$SCRIPT_DIR":/ci-docker --entrypoint /ci-docker/pipeline-kernel-aw32.sh retrolx/build-environment-ubuntu-20.10
+docker run -v "$SCRIPT_DIR":/ci-docker --add-host invisible-mirror.net:1.1.1.1 --add-host mirror.keystealth.org:1.1.1.1 --entrypoint /ci-docker/pipeline-kernel-aw32.sh retrolx/build-environment-ubuntu-20.10 > allout.log 2>&1 &
