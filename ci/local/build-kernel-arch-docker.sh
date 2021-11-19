397c4b4fd995ef6f8dfe82ee9b10c059df61485b
lilo <lilo@lilo-co.almostmy.com>
2021-11-19 23:37:29 +0100

    CI WIP by Lilo

--- ci/local/build-kernel-arch-docker.sh
+++ ci/local/build-kernel-arch-docker.sh
@@ -3,8 +3,10 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$SCRIPT_DIR"
+REPOSITORY="$SCRIPT_DIR/../../"
+echo "$REPOSITORY"
 echo "Architecture $1"
 
 docker pull retrolx/build-environment-ubuntu-20.10
-docker run --entrypoint /ci-docker/build-kernel-arch.sh -v "$SCRIPT_DIR":/ci-docker --add-host invisible-mirror.net:1.1.1.1 --rm retrolx/build-environment-ubuntu-20.10 $1 > allout.log 2>&1 &
+docker run --entrypoint /ci-docker/build-kernel-arch.sh -v "$REPOSITORY":/kernel --add-host invisible-mirror.net:1.1.1.1 --rm retrolx/build-environment-ubuntu-20.10 $1 > allout.log 2>&1 &
 tail -f allout.log
--- ci/local/build-kernel-arch.sh
+++ ci/local/build-kernel-arch.sh
@@ -3,9 +3,16 @@
 
 date
 printenv
-cd /ci-docker # This line will work on docker and fail localy
-git clone https://github.com/RetroLX/kernel.git
-cd kernel
+
+if [ -d "/kernel" ]; then # Docker mounts the repository on this directory
+  cd /kernel
+else
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    REPOSITORY="$SCRIPT_DIR/../../"
+    cd $REPOSITORY
+    pwd
+fi
+
 git submodule init
 git submodule update
 make kernel-$1-build
