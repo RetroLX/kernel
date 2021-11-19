e4c21080d249fc51f5a7daf32e9370f12209e2e4
lilo <lilo@lilo-co.almostmy.com>
2021-11-19 12:47:43 +0100

    CI WIP by Lilo

--- ci/local/build-kernel-arch-docker.sh
+++ ci/local/build-kernel-arch-docker.sh
@@ -6,4 +6,4 @@ echo "$SCRIPT_DIR"
 echo "Architecture $1"
 
 docker pull retrolx/build-environment-ubuntu-20.10
-docker run -v "$SCRIPT_DIR":/ci-docker --add-host invisible-mirror.net:1.1.1.1 --rm retrolx/build-environment-ubuntu-20.10 --entrypoint /ci-docker/build-kernel-arch.sh $1 > allout.log 2>&1 &
+docker run --entrypoint /ci-docker/build-kernel-arch.sh -v "$SCRIPT_DIR":/ci-docker --add-host invisible-mirror.net:1.1.1.1 --rm retrolx/build-environment-ubuntu-20.10 $1 > allout.log 2>&1 &
