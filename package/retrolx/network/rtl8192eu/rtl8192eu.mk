304e578477e714acbaaccec845bd93936a3f4065
Romain TISSERAND <romain.tisserand@gmail.com>
2021-12-01 23:07:18 +0100

    Bring back RTL8192EU 5.15 oot module (at least for H616)

--- package/retrolx/network/rtl8192eu/rtl8192eu.mk
+++ package/retrolx/network/rtl8192eu/rtl8192eu.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-RTL8192EU_VERSION = 546419675335bb80d4548e22ccef915573a5a1e2
-RTL8192EU_SITE = $(call github,Mange,rtl8192eu-linux-driver,$(RTL8192EU_VERSION))
+RTL8192EU_VERSION = fb81d860ea4f6d54bfc2a9a8f1aa5c37eb6aea6b
+RTL8192EU_SITE = $(call github,clnhub,rtl8192eu-linux,$(RTL8192EU_VERSION))
 RTL8192EU_LICENSE = GPL-2.0
 RTL8192EU_LICENSE_FILES = LICENSE
 
 RTL8192EU_MODULE_MAKE_OPTS = \
 	CONFIG_RTL8192EU=m \
+	TopDIR=$(@D) \
 # batocera: setting KVER breaks top level parallelization
 	# KVER=$(LINUX_VERSION_PROBED)
 	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
@@ -20,7 +21,7 @@ define RTL8192EU_MAKE_SUBDIR
         (cd $(@D); ln -s . rtl8192eu)
 endef
 
-RTL8192eu_PRE_CONFIGURE_HOOKS += RTL8192EU_MAKE_SUBDIR
+#RTL8192eu_PRE_CONFIGURE_HOOKS += RTL8192EU_MAKE_SUBDIR
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
